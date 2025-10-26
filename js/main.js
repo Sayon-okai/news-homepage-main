f18936d2d0b1d9520a499296b49a91299fa69805
Sayon-okai <sayon.study@gmail.com>
2025-10-26 16:06:05 +0000

    nav menu added

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@
 function showSideBar() {
     $(".sidebar").css("display", "flex");
 }
+
+function hideSideBar() {
+    $(".sidebar").css("display", "none");
+}
+
+
+if ($(".sidebar").css() === "display: none") {
+    document.reload();
+}
+
+
+
